0b75485f13dad7bcacb5399c9a49cdb8bcd6758b
Karen <karen@mercosys.(none)>
2013-01-22 12:53:59 -0200

    validates metas

--- app/models/meta.rb
+++ app/models/meta.rb
@@ -1,18 +1,9 @@
 class Meta < ActiveRecord::Base
   attr_accessible :descricao, :moeda, :periodo_final, :periodo_inicio, :status, :valor_max_gs, :valor_max_rs, :valor_max_us, :valor_min_gs, :valor_min_rs, :valor_min_us
 
-  has_many :meta_detalhes
-
-  def finds
-
-  	#busca empleado
-	  per = Persona.find_by_id(self.persona_id);
-	  self.persona_nome = per.nome.to_s unless self.persona_id.blank?
-
-	  #busca setor
-	  set = Setors.find_by_id(self.persona_id);
-	  self.persona_nome = set.nome.to_s unless self.persona_id.blank?
-  end
+  validates :moeda, :periodo_final, :periodo_inicio, :status, :presence => true
 
   
+  has_many :meta_detalhes
+
 end
--- app/models/meta_detalhe.rb
+++ app/models/meta_detalhe.rb
@@ -1,6 +1,9 @@
 class MetaDetalhe < ActiveRecord::Base
   attr_accessible :comicao_max, :comicao_min, :descricao, :meta_id, :persona_id, :persona_nome, :setor_id, :setor_nome, :valor_max_gs, :valor_max_rs, :valor_max_us, :valor_min_gs, :valor_min_rs, :valor_min_us
 
+  validates :comicao_max, :comicao_min , :numericality =>  { :greater_than => 0 }
+  validates :comicao_max, :comicao_min, :persona_id, :persona_nome, :setor_id, :setor_nome, :presence => true
+
   belongs_to :metas
   has_many :setors
   has_many :personas
